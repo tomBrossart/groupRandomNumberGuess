7c8ee3cc2742f3b7839b8d3150c17b4d650b6bc7
Tom Brossart <tom@blueraven.digital>
2017-06-29 17:03:37 -0500

    added some functionality to client.js  and server.js

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,3 +1,22 @@
 $(document).ready(function() {
   console.log('nice and easy');
+  // on document load just show setup mode
+  $('#play-mode').hide();
+
+  // take user radio selection with click listener and pass to server
+  $('#start-game').on('click', function(){
+    var selection = $('radio').val();
+  });
+
+
+
+  // $.ajax({
+  //   type: 'POST',
+  //   url: '/',   // address route for getting/putting data from server
+  //   success: function(response) {
+  //     console.log("request received");
+  //     // connect up button
+  //     }
+  //   });
+
 });
--- server/server.js
+++ server/server.js
@@ -1,10 +1,17 @@
 var express = require('express');
 var app = express();
+var randomNumber = require('./modules/randomNumber.js');
 
 var path = require('path');
 var port = 8000;
 var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/setup", function (req, res){
+  var rN = randomNumber(100, 400);
+  res.send(rN);
+  console.log(rN);
+});
 
 app.get('/*', function(req, res){
   var file = req.params[0] || '/views/index.html';
