a67d5cd37681df9ac3460d8fe9cee223a7930941
Tom Brossart <tom@blueraven.digital>
2017-06-30 10:05:49 -0500

    got maxNum to show on console

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,3 +1,7 @@
+var maxNum = 0;
+
+
+
 $(document).ready(function() {
   console.log('nice and easy');
   // on document load just show setup mode
@@ -5,18 +9,26 @@ $(document).ready(function() {
 
   // take user radio selection with click listener and pass to server
   $('#start-game').on('click', function(){
+    $('#setup-mode').hide();
+    $('#play-mode').show();
+    // selecting maxnumber from setup and setting as var
+    maxNum = $('input:radio[name=max]:checked').val();
+    $('#maximum-number').text(' ' + maxNum);
     var selection = $('radio').val();
+    $.ajax({
+      type: 'POST',
+      url: '/maxNum',   // address route for getting/putting data from server
+      data: {maxNum: maxNum},
+      success: function(response) {
+        console.log("request received");
+        console.log(response);
+        // connect up button
+        }
+      });
   });
 
 
 
-  // $.ajax({
-  //   type: 'POST',
-  //   url: '/',   // address route for getting/putting data from server
-  //   success: function(response) {
-  //     console.log("request received");
-  //     // connect up button
-  //     }
-  //   });
+
 
 });
--- server/server.js
+++ server/server.js
@@ -7,11 +7,21 @@ var port = 8000;
 var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.get("/setup", function (req, res){
-  var rN = randomNumber(100, 400);
-  res.send(rN);
-  console.log(rN);
+
+app.post("/maxNum", function (req, res){
+  var mN = req.body;
+  // var rN = randomNumber(100, 400);
+  console.log('received max  Number:', mN.maxNum);
+  // we sent back the mN object with the .maxNum property which logged the selected maxNumber
+  res.send('' + mN.maxNum); //MAKE STRING
 });
+
+//  TESTING THIS THURSDAY
+// app.get("/setup", function (req, res){
+//   var rN = randomNumber(100, 400);
+//   console.log(rN);
+//   res.send('' + rN); //MAKE STRING
+// });
 
 app.get('/*', function(req, res){
   var file = req.params[0] || '/views/index.html';
